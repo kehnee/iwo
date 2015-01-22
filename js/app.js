cc7bc7d4cdb53c63978cd3ce786ba30ef80ead17
Ahmed <hafizahmedattari@gmail.com>
2015-01-22 21:33:09 +0500

    Updated new changes.

--- js/app.js
+++ js/app.js
@@ -1,5 +1,101 @@
-angular.module('App', ['ionic', 'App.controllers', 'App.services'])
-
+angular.module('App', ['ionic', 'config', 'App.controllers', 'App.services'])
+    .config(function ($stateProvider, $urlRouterProvider, ConfigProvider) {
+        ConfigProvider
+            .authState('app.browse')
+            .unAuthState('login')
+            .loginService('isLogin');
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'templates/login.html',
+                controller: 'loginCtrl',
+                authenticate: false
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: 'templates/register.html',
+                controller: 'registerCtrl',
+                authenticate: false
+            })
+            .state('forgotPassword', {
+                url: '/forgotPassword',
+                templateUrl: 'templates/forgotPassword.html',
+                controller: 'forgotPasswordCtrl',
+                authenticate: false
+            })
+            .state('app', {
+                abstract: true,
+                templateUrl: "templates/menu.html"
+            })
+            .state('app.search', {
+                url: "/search",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/search.html"
+                    }
+                },
+                authenticate: true
+            })
+            .state('app.browse', {
+                url: "/browse",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/browse.html"
+                    }
+                },
+                authenticate: true
+            })
+            .state('app.minister', {
+                url: "/minister",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/minister.html"
+                    }
+                },
+                authenticate: true
+            })
+            .state('app.profile', {
+                url: "/profile",
+                views: {
+                    'menuContent': {
+                        controller: "profileCtrl",
+                        templateUrl: "templates/profile.html"
+                    }
+                },
+                authenticate: true
+            })
+            .state('app.purchase', {
+                url: "/purchase",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/purchase.html"
+                    }
+                },
+                authenticate: true
+            })
+            .state('app.singleProductView', {
+                url: "/singleProductView",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/singleProductView.html"
+                    }
+                },
+                authenticate: true
+            })
+            .state('app.seriesProductView', {
+                url: "/seriesProductView",
+                views: {
+                    'menuContent': {
+                        templateUrl: "templates/seriesProductView.html"
+                    }
+                },
+                authenticate: true
+            })
+        ;
+        // if none of the above states are matched, use this as the fallback
+        $urlRouterProvider.otherwise('/browse');
+    })
+    .constant('BASE_URL','http://iworshiponline.com')
     .run(function ($ionicPlatform, $rootScope, $state) {
         $state.back = function () {
             $state.go($state.previous||"app.browse")
@@ -7,7 +103,7 @@ angular.module('App', ['ionic', 'App.controllers', 'App.services'])
         $rootScope.$on('$stateChangeStart', function (event, toState, toParams, fromState, fromParams) {
             $state.previous = fromState.name;
         });
-            $ionicPlatform.ready(function () {
+        $ionicPlatform.ready(function () {
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -19,84 +115,5 @@ angular.module('App', ['ionic', 'App.controllers', 'App.services'])
             }
         });
     })
-
-    .config(function ($stateProvider, $urlRouterProvider) {
-        $stateProvider
-            .state('login', {
-                url: '/login',
-                templateUrl: 'templates/login.html',
-                controller: 'loginCtrl'
-            })
-            .state('register', {
-                url: '/register',
-                templateUrl: 'templates/register.html',
-                controller: 'registerCtrl'
-            })
-            .state('app', {
-                abstract: true,
-                templateUrl: "templates/menu.html",
-                controller: 'mainCtrl'
-            })
-            .state('app.search', {
-                url: "/search",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/search.html"
-                    }
-                }
-            })
-            .state('app.browse', {
-                url: "/browse",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/browse.html"
-                    }
-                }
-            })
-            .state('app.minister', {
-                url: "/minister",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/minister.html"
-                    }
-                }
-            })
-            .state('app.profile', {
-                url: "/profile",
-                views: {
-                    'menuContent': {
-                        controller: "profileCtrl",
-                        templateUrl: "templates/profile.html"
-                    }
-                }
-            })
-            .state('app.purchase', {
-                url: "/purchase",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/purchase.html"
-                    }
-                }
-            })
-            .state('app.singleProductView', {
-                url: "/singleProductView",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/singleProductView.html"
-                    }
-                }
-            })
-            .state('app.seriesProductView', {
-                url: "/seriesProductView",
-                views: {
-                    'menuContent': {
-                        templateUrl: "templates/seriesProductView.html"
-                    }
-                }
-            })
-        ;
-        // if none of the above states are matched, use this as the fallback
-        $urlRouterProvider.otherwise('/login');
-    });
 angular.module('App.controllers', []);
 angular.module('App.services', []);
--- js/controllers.js
+++ js/controllers.js
@@ -1,18 +1,93 @@
 angular.module('App.controllers')
+    .controller('loginCtrl', function ($scope, $state, User) {
+        $scope.user={};
 
-    .controller('loginCtrl', function ($scope) {
-
+        $scope.login = function (form) {
+            if(form.$invalid) return;
+            User.login($scope.user,function(){
+                $scope.$emit('user');
+                $state.go('app.browse');
+            },function(err){
+                if(err&&typeof err!=="string") err=!1;
+                err&&$scope.$emit('Alert', {
+                    type:'error',
+                    msg: err
+                });
+            });
+        };
     })
-    .controller('registerCtrl', function ($scope) {
+    .controller('registerCtrl', function ($scope, $state, User) {
+        $scope.user={};
 
-    })
-    .controller('profileCtrl', function ($scope, mainService) {
-        $scope.user=mainService.user;
-    })
+        $scope.getAvatar = function (img) {
+            var file=img[0];
+            if(!file||!/image/.test(file.type)) return;
+            var reader = new FileReader();
+            reader.onload = function () {
+                $scope.user.avatar = reader.result;
+                $scope.$$phase || $scope.$digest()
+            };
+            reader.readAsDataURL(file);
+        };
 
-    .controller('mainCtrl', function ($scope, $ionicActionSheet, $state) {
+        $scope.register = function (form) {
+            if(form.$invalid) return;
+            User.register($scope.user,function(){
+                $scope.$emit('Alert', {
+                    type:'success',
+                    msg: 'Registered Successful'
+                }, function () {
+                    $state.go('login');
+                });
+            },function(err){
+                if(err&&typeof err!=="string") err=!1;
+                err&&$scope.$emit('Alert', {
+                    type:'error',
+                    msg: err
+                });
+            });
+        };
+    })
+    .controller('forgotPasswordCtrl', function ($scope,User) {
+        $scope.user={};
+
+        $scope.forgotPassword = function(form){
+            if(form.$invalid) return;
+            User.forgotPassword($scope.user.email, function(msg){
+                $scope.$emit('Alert', {
+                    type:'success',
+                    msg: msg
+                });
+            }, function(err){
+                if(err&&typeof err!=="string") err=!1;
+                err&&$scope.$emit('Alert', {
+                    type:'error',
+                    msg: err
+                });
+            })
+        };
+    })
+    .controller('profileCtrl', function ($scope) {
+    })
+    .controller('mainCtrl', function ($rootScope, $scope, $ionicActionSheet, $http, $ionicLoading, $ionicPopup, $state, User) {
         $scope.goBack = function() {
             $state.back();
+        };
+        function getUser(){
+            if(!User.getUser()){
+                User.getUserInfo(function (user) {
+                    $scope.user=user;
+                    $scope.$$phase||$scope.$digest();
+                });
+            } else {
+                $scope.user=User.getUser();
+                $scope.$$phase||$scope.$digest();
+            }
+        }
+        getUser();
+
+        $scope.logout=function () {
+            $scope.$emit('user',true);
         };
 
         $scope.showActionSheet = function() {
@@ -33,6 +108,50 @@ angular.module('App.controllers')
                     return true;
                 }
             });
-
         };
+
+        $rootScope.$on('user', function(e, logout){
+            if(logout) {
+                User.logout();
+                $scope.user=!1;
+                $state.go('login');
+            }
+            else getUser();
+            $scope.$$phase||$scope.$digest();
+        });
+
+        $rootScope.$on('Alert',function(e,opt, cb){
+            if(typeof cb!=="function") cb=function(){};
+            if(!opt) return;
+            var type='positive';
+            if(opt.type&&typeof opt.type==="string"){
+                if(opt.type.toLowerCase()==='error') {
+                    type = 'assertive';
+                    opt.title='Error';
+                }
+                if(opt.type.toLowerCase()==='success') opt.title='Success';
+            }
+            var error = $ionicPopup.alert({
+                title: '<span class="'+type+'">'+(opt.title||'Alert')+'</span>',
+                template: opt.msg||'Unknown error.',
+                okType: 'button-'+type
+            });
+            error.then(cb);
+        });
+
+        $scope.$watch(function() {
+            var p =$http.pendingRequests;
+            var r = p.filter(function(r, i) {
+                var urls = [];
+                return !(urls.some(function(u) {
+                    return r.url === u || r.url === location.origin + u
+                }))
+            });
+            return r.length > 0;
+        }, function(v) {
+            if (v) $ionicLoading.show({
+                template: '<i class="button-icon icon ion-loading-c"></i>'
+            });
+            else $ionicLoading.hide();
+        });
     });
